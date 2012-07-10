2016c48071fabe3dab26d05e40e4c0c73a6b6ff6
Stephen Finney <stephen@codeforamerica.org>
2012-07-09 21:28:04 -0400

    Fixed typos

--- lib/models/parkCalc.rb
+++ lib/models/parkCalc.rb
@@ -8,9 +8,9 @@ class ParkingCalc < ActiveRecord::Base
 
       bizName 	  = row[0]
       catName 	  = row[1]
-      ruleList 	  = Array.new(row[2].split(/;*/)
-      noteList 	  = Array.new(row[3].split(/;*/)
-      calcPrompt  = Array.new(row[4].split(/;*/)
+      ruleList 	  = Array.new( row[2].split(/;*/) )
+      noteList 	  = Array.new( row[3].split(/;*/) )
+      calcPrompt  = Array.new( row[4].split(/;*/) )
 
       biz = ParkingCalc.new(:name => bizName, :cat_name => catName, :rules => ruleList,
 			    :notes => noteList, :calc_prompt => calcPrompt)
