34d1f07b572498a085cb3666a6b773a2822af465
Ruthie BenDor <ruthie@unruthless.com>
2012-07-30 16:46:22 -0700

    Added phone and role fields to Users table

---
+++ db/migrate/20120730234147_add_role_to_users.rb
@@ -0,0 +1,7 @@
+class AddRoleToUsers < ActiveRecord::Migration
+  def change
+
+    add_column :users, :role, :string
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730221215) do
+ActiveRecord::Schema.define(:version => 20120730234147) do
 
   create_table "field_answers", :force => true do |t|
     t.text     "answer"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(:version => 20120730221215) do
     t.string   "token"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "phone"
+    t.string   "role"
   end
 
 end
