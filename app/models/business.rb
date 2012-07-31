741609d2e35c07115fdc255ff7f1fd0353b35fee
Samuel Painter <sam.foy.painter@gmail.com>
2012-07-31 14:41:51 -0400

    associated naics codes with business

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,21 @@
 class Business < ActiveRecord::Base
   has_and_belongs_to_many :users
-  attr_accessible :description, :is_home_occ, :is_sole_owner, :mailing_address_city, :mailing_address_detail, :mailing_address_state, :mailing_address_street, :mailing_address_zip, :name, :phone, :physical_address_city, :physical_address_detail, :physical_address_state, :physical_address_street, :physical_address_zip, :structure, :type
+  has_one :naics
+  attr_accessible :description, 
+                  :is_home_occ, 
+                  :is_sole_owner, 
+                  :mailing_address_city, 
+                  :mailing_address_detail, 
+                  :mailing_address_state, 
+                  :mailing_address_street, 
+                  :mailing_address_zip, 
+                  :name, 
+                  :phone, 
+                  :physical_address_city, 
+                  :physical_address_detail, 
+                  :physical_address_state, 
+                  :physical_address_street, 
+                  :physical_address_zip, 
+                  :structure, 
+                  #:type I assume this is covered by the naics code
 end
