21ed88d5c97fe410e7ffccba2d990f4851a2c34b
hampelm <hampelm>
2012-08-04 09:28:04 -0700

    Add missing comma in payrol_tax attr_accessible list

--- app/models/payroll_tax.rb
+++ app/models/payroll_tax.rb
@@ -1,7 +1,7 @@
 class PayrollTax < ActiveRecord::Base
   belongs_to :business #we can get owner info from here
   
-  attr_accessible :set_state_id, :quarter_wage_over_100 :year_wage_over_100, :first_payroll_date, :application_type, :purchased_biz, :previous_owner_first, :previous_owner_last, :previous_eed_account_number, :date_of_transfer, :california_employees, :taxpayer_type, :employer_type, :is_employee_family, :industry_type, :other, :contact_first, :contact_last, :contact_street, :contact_detail, :contact_state, :contact_city, :contact_zip, :contact_phone, :contact_email
+  attr_accessible :set_state_id, :quarter_wage_over_100, :year_wage_over_100, :first_payroll_date, :application_type, :purchased_biz, :previous_owner_first, :previous_owner_last, :previous_eed_account_number, :date_of_transfer, :california_employees, :taxpayer_type, :employer_type, :is_employee_family, :industry_type, :other, :contact_first, :contact_last, :contact_street, :contact_detail, :contact_state, :contact_city, :contact_zip, :contact_phone, :contact_email
 
 
   validates_format_of :phone, 
