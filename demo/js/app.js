7da206e4b037daba11152dbf6988c85f755dd5cb
Mick Thompson <dthompson@gmail.com>
2012-08-14 14:00:50 -0700

    add in demo folder from gh-pages for convenience... #temporary

---
+++ demo/js/app.js
@@ -0,0 +1,226 @@
+var OC = { 
+  
+  prefix: 'OC-',
+  
+  forms: {},
+  util: {},
+  state: {},
+  calculator: {},
+  data: {},
+  
+  init: function() {
+    console.log("Initializing");
+    
+    console.log("Here is everything in localStorage:");
+    console.log(localStorage);
+    
+    OC.routing.init();
+    OC.location.init();
+    OC.calculator.parking.general.init();
+    $('.submit').bind({
+      click: OC.forms.submitLink
+    });
+    
+    OC.util.isPlanningOpen();
+    
+    // Update first name when you hit business-info'. 'first-name' 
+        
+    // Check to see if we have stored data about any forms
+    OC.forms.recallFields();
+
+    // Autocomplete business types
+    $('#business_code').autocomplete({
+      source: OC.data.calgoldBusinessTypes,
+      appendTo: '#business-type-container',
+      select: function(event, ui) {
+        // Save the selected option (doesn't happen naturally the way we want)
+        key = OC.forms.key('naics');
+        OC.util.storeData(key, ui);
+      }
+    });
+    
+    // Set up we're-here-to-help toggles
+    $('.drawer').hide();
+    $('.toggle').click(function(e){
+      e.PreventDefault;
+      $(this).next('.drawer').toggle();
+    });
+    
+    // Listen and be ready to update some panels
+    $.subscribe("updatePanel-info_business", function(event){
+      $('.first-name').html(OC.util.getData('OC-field-applicant_first_name'));
+    });
+    
+    $.subscribe("updatePanel-location_non_home_occ_check_site", function(event){
+      var businessCode = OC.util.getData('OC-field-naics');
+      console.log(businessCode);
+      
+      $('.business-type').html(businessCode.item.value);
+      $('.business-naics-code').html(businessCode.item.name);
+    });
+    
+    
+    
+    $.subscribe("updatePanel-location_non_home_occ_choose_site", function(event){
+      OC.calculator.zoning.init();
+    });
+    
+    
+    // Set up Profile pulldown
+    $('.profile-contents').hide();
+    $('.profile-toggle').click(function(e){
+        // Draw .profile-contents
+        $('.profile-contents').slideToggle();
+    });
+    
+    // Set up conditional mailing address on Working From Home panel
+    $('input[name="does_mailing_address_match_business_address"]').parent().next('.conditional').hide();
+    $('input[name="does_mailing_address_match_business_address"]').live("click", function(){
+        if (!($(this).is(':checked'))) {
+            $(this).parent().next('.conditional').show();
+        } else {
+            $(this).parent().next('.conditional').hide();
+        }
+    });
+    
+    // Set up conditional fields on About Your Business panel
+    $('input[name="applicant_is_sole_owner"]').parent().next('.conditional').hide();
+    $('input[name="applicant_is_sole_owner"]').live("click", function(){
+        if (!($(this).is(':checked'))) {
+            $(this).parent().next('.conditional').show();
+        } else {
+            $(this).parent().next('.conditional').hide();
+        }
+    });
+    
+    // Set up conditional fields on Building Permit panel
+
+
+  }
+};
+
+/** 
+ * Fill in any fields with saved data
+ */
+OC.forms.recallFields = function() {
+  $('input').each(function(index){
+    var name = $(this).attr('name');
+
+    var key = OC.forms.key(name);
+    var value = OC.util.getData(key);
+    var type = $(this).attr('type');
+  
+
+    //if(value) { console.log(value); }
+    
+    if (type === 'radio') {
+      if($(this).attr('value') == value) {
+        $(this).prop("checked", true);
+      }
+    }else if(type === 'checkbox') {
+      $(this).prop("checked", true);
+    }else if(type !='submit'){
+      $(this).val(value);
+    };
+    
+  });
+};
+
+OC.forms.businessTypeAutocomplete = function(event) {
+  var value = $(this).attr('value');
+  console.log(value);
+  
+  // Autocomplete stuff
+  // #businessTypeInput
+};
+
+/** 
+ * Generate a key for localStorage based on the name of a form field
+ */ 
+OC.forms.key = function(name) {
+  return OC.prefix + 'field-' + name;
+};
+
+/**
+ * Handle the sumbit link being clicked
+ */
+OC.forms.submitLink = function(event) {
+  event.preventDefault();
+  var form = $(this).closest('form');
+  OC.forms.sumbit(form);
+};
+
+/**
+ * Handle form submissions our way
+ */
+OC.forms.sumbit = function(form) {  
+  // Save the data from the form in localStorage
+  var data = $(form).serializeObject();
+  console.log(form);
+  
+  $.each(data, function(key, value){
+    var localStorageKey = OC.forms.key(key);
+    console.log("Key: " + key + " value: " + value);
+    OC.util.storeData(localStorageKey, value);
+  });
+};
+
+/** .........................................................................
+ *  Utilities
+ */
+
+OC.state.set = function(key, val){
+    var data = OC.util.getData("OC-state");
+    if(data === null){
+        data = {};
+    }
+    data[key] = val;
+    OC.util.storeData("OC-state", data);
+};
+
+OC.state.get = function(key){
+
+    var data = OC.util.getData("OC-state");
+    if(data === null){
+        data = {};
+    }
+    if(key === undefined){
+        return data;
+    }else{
+        return data[key];
+    }
+};
+ 
+OC.util.getData = function(key) {
+    return JSON.parse(localStorage.getItem(key));
+};
+
+OC.util.storeData = function(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+OC.util.isPlanningOpen = function() {
+  // Todo: tell if planning is actually open.
+  return "closed";
+};
+
+/** 
+ * Utility to serialize complex things. Like forms.
+ * http://stackoverflow.com/a/1186309/117014  
+ * usage: $('form').serializeObject();
+ */
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
---
+++ demo/js/routing.js
@@ -0,0 +1,109 @@
+OC.routing = {};
+
+OC.routing.handleClick = function(){
+
+
+    $(document).on("click", "a:not([data-bypass])", function(evt) {
+        // Get the absolute anchor href.
+        var href = $(this).attr("href");
+        var protocol = this.protocol + "//";
+
+        // Ensure the protocol is not part of URL, meaning it's relative.
+        if (href && href.slice(0, protocol.length) !== protocol &&
+            href.indexOf("javascript:") !== 0  &&
+            href.indexOf("mailto:") !== 0) {
+
+            hash = href.replace(/^[#|\/]/, "");
+            evt.preventDefault();
+            OC.routing.handleLinkData(this);
+            OC.routing.findPlaceInNav(hash);
+            OC.routing.navigate(hash, true);
+        };
+    });
+};
+
+OC.routing.init = function(){
+    OC.routing.reloadState();
+    OC.routing.handleClick();
+    window.onpopstate = function(event){
+        if(window.location.hash == ""){
+            // we are home. show welcome
+            OC.routing.showPanel("intro");
+            OC.routing.findPlaceInNav("intro");
+        }else{
+            var hash = window.location.hash.replace(/^\#/, "");
+            OC.routing.showPanel(hash);
+        };
+    };
+};
+
+OC.routing.findPlaceInNav = function(hash){
+    $("nav.nav-main li[data-section]").hide();
+    $("nav.nav-main a").each(function(c, el){
+        $(el).parent().removeClass("current");
+        if($(el).attr("href") && $(el).attr("href").substr(1) == hash){
+            $(el).parent().addClass("current");
+            if($(el).is(".section_heading")){
+                var dataSection = $(el).attr("href").substr(1);
+            }else{
+                var dataSection = $(el).parents("nav>ul>li").prev().find("a").attr("href").substr(1);
+            }
+            if(dataSection !== "location" || OC.state.get("occupancy") !== undefined){
+                $("li[data-section="+dataSection+"]").show();
+            }
+            OC.state.set("section", dataSection);
+        }
+    });
+};
+
+OC.routing.dataAttrs = ["data-occupancy", "data-zoning-status", "data-parking-district", "data-section"];
+
+OC.routing.handleLinkData = function(el){
+    for(d in OC.routing.dataAttrs){
+        var attr = OC.routing.dataAttrs[d];
+        if($(el).is("["+attr+"]")){
+            var val = $(el).attr(attr);
+            OC.routing.setDataAttrState(attr, val);
+        }
+    };
+};
+OC.routing.setDataAttrState = function(attr, val){
+    OC.state.set(attr.substr(5), val);
+    $("["+attr+"]:not(["+attr+"="+val+"]):not(a)").hide();
+    $("["+attr+"="+val+"]").show();
+}
+
+
+OC.routing.stateDefaults = {"zoning-status":"none", "parking-district":"none", "section":"intro"};
+
+OC.routing.reloadState = function(){
+    var state = OC.state.get();
+    for(d in OC.routing.stateDefaults){
+        if(state[d] === undefined){
+            state[d] =  OC.routing.stateDefaults[d];
+        }
+    }
+    for(s in state){
+        $("[data-"+s+"]:not([data-"+s+"="+state[s]+"]):not(a)").hide();
+        $("[data-"+s+"="+state[s]+"]").show();
+    }
+};
+
+OC.routing.updatePanel = function(panel) {
+  $(panel).find('.first-name').html(OC.util.getData('OC-field-applicant_first_name'));
+};
+
+OC.routing.showPanel = function(panelId){
+    console.log("Panel " + panelId);
+    $("section.content > div").hide();
+    $("div#"+panelId).show();
+    $.publish("updatePanel-" + panelId, [ panelId ]);
+    
+    // OC.routing.updatePanel($("div#"+panelId));
+};
+
+OC.routing.navigate = function(hash, addToHistory){
+    OC.routing.showPanel(hash);
+    // add to history
+    history.pushState({"page":hash}, hash, "#"+hash);
+};
