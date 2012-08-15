8a11fe71a66fd93e80567b5a469d6a931513c028
Mick Thompson <dthompson@gmail.com>
2012-08-14 17:33:29 -0700

    semi working route to templates

--- backbone/app/router.js
+++ backbone/app/router.js
@@ -1,21 +1,43 @@
 define([
   // Application.
-  "app"
+  "app",
+  "modules/user",
+  "modules/business",
+  "modules/answers"
 ],
 
-function(app) {
+function(app, User, Business, Answers) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
     routes: {
-      "": "index"
+      "":"index",
+      "*path":"panel"
     },
+    index: function(e){
+      console.log("index");
+    },
+    panel: function(path){
+      console.log("panel: ", path);
+      app.layout.setViews({
+        "#cpan": new Answers.Views.Panel({
+          collection:this.answers,
+          // this input needs to be scrubbed if bb doesnt already - Mick
+          useTemplate:"panels/"+path
+        })
+      });
 
-    index: function() {
+      this.answers.reset();
+    },
+    initialize: function(){
+      this.user = new User.Model();
+      this.business = new Business.Model();
+      this.answers = new Answers.Collection();
 
-    }
-  });
+      app.useLayout("main").render();
 
+      //these should be a collection
+    }});
   return Router;
 
 });
