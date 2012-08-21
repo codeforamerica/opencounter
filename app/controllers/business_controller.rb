af9de1ebb5b6c2f1b74a2dfc2530feafeabe5751
Alex Yule <alexy@codeforamerica.org>
2012-08-21 16:28:55 -0700

    Basic controller code to create/update business objects

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,2 +1,31 @@
 class BusinessController < ApplicationController
+  def create
+    @biz = Business.new(params[:business])
+    respond_to do |format|
+      if @biz.save
+        #format.html { redirect_to @biz, notice: 'Business was successfully created.' }
+        format.json { render json: @biz, status: :created, location: @biz }
+      else
+        #format.html { render action: "new" }
+        format.json { render json: @biz.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /business/1
+  # PUT /business/1.json
+  def update
+    @biz = Business.find(params[:id])
+
+    respond_to do |format|
+      if @biz.update_attributes(params[:business])
+#        format.html { redirect_to @biz, notice: 'Business was successfully updated.' }
+        format.json { head :no_content }
+      else
+#        format.html { render action: "edit" }
+        format.json { render json: @biz.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
 end
