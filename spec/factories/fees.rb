83ae3d813fe25f232c3c655f9eeb39450b0a05fb
Alex Yule <alexy@codeforamerica.org>
2012-08-21 18:48:11 -0700

    Continuing cleanup from renaming of FieldAnswers to Answers, and removal of Fees

--- spec/factories/fees.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :fee, :class => 'Fees' do
-    conditions "MyText"
-    formula "MyText"
-    payable_to "MyText"
-  end
-end
--- spec/helpers/answers_helper_spec.rb
+++ spec/helpers/answers_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe FieldAnswersHelper do
+describe AnswersHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
