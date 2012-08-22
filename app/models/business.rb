0b86fbb74c17bcceb1687c954decd7b5e022ed1c
Alex Yule <alexy@codeforamerica.org>
2012-08-21 18:48:36 -0700

    Adding business controller stubs and tests

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,11 @@
 class Business < ActiveRecord::Base
   belongs_to :user
   has_many :answers
-  attr_accessible :name, :description, :answers_attributes
-  accepts_nested_attributes_for :answers, :reject_if => :all_blank
+  attr_accessible :name, :description
+
+  # answers are a hash of key/value pairs
+  def save_answers(answers)
+
+
+  end
 end
