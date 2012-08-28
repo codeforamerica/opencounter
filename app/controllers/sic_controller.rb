880b037d054415513476ebda8991022812fb9200
Aurelio Tinio <aurelio@tinio.org>
2012-08-28 12:21:12 -0700

    add all csv fields

--- app/controllers/sic_controller.rb
+++ app/controllers/sic_controller.rb
@@ -4,7 +4,7 @@ class SicController < ApplicationController
   def index
     results = CsvMapper.import(Rails.root + 'app/assets/csv/zoning_and_sic_codes.csv') do
       start_at_row 1
-      [industry, subtype, code]
+      [industry, subtype, code, industry_subtype, recheck, sic_name, sic_code, alternate_code]
     end
 
     @response = results.first
