4d82d7d9fbca5d44ec501a0278f6192790e6f3a0
Mick Thompson <dthompson@gmail.com>
2012-08-28 10:23:05 -0700

    add tests for business create

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,28 +1,23 @@
-class BusinessController < ApplicationController
+class BusinessesController < ApplicationController
+  respond_to :json
+  
   # GET /business/new
   def create
-    @biz = Business.new(params[:business])
-    respond_to do |format|
-      if @biz.save
-        #format.html { redirect_to @biz, notice: 'Business was successfully created.' }
-        format.json { render json: @biz, status: :created, location: @biz }
-      else
-        #format.html { render action: "new" }
-        format.json { render json: @biz.errors, status: :unprocessable_entity }
-      end
-    end
+    @biz = Business.create(params[:business])
+    respond_with @biz
   end
 
   # PUT /business/1
   # PUT /business/1.json
   # Expected structure: 
   # {"business": {"name": "value"}, "answers": { "key": "value", "key": "value"}}
+
   # TODO: change structure over to nested, make sure to copy params after pulling out answers so @biz.update_attrs doesn't fail
   # OR push everything to biz and override the business.answers setter if rails does that
   def update
     @biz = Business.find(params[:id])
     errors = [] 
-    errors << @biz.save_answers(params[:answers]) if params[:answers]
+    errors << @biz.save_answers(params[:business][:answers]) if params[:business][:answers]
     errors << @biz.update_attributes(params[:business]) if params[:business]
 
     respond_to do |format|
--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -3,9 +3,37 @@ require 'spec_helper'
 describe BusinessesController do
 
   describe "#create" do
-    it "creates a poll" do
-      post :create, :business=> FactoryGirl.attributes_for(:business)
-      assigns[:business].should_not be_nil
+    let(:name){
+      "test biz"
+    }
+    let(:desc){
+      "test biz"
+    }
+    let(:business){
+      {:name => name, :description => desc}
+    }
+
+    
+    it "creates a business" do
+      post :create, :business=> business, :format => :json
+
+      response.code.should == '201'
+      resp_json = JSON.parse response.body
+      p resp_json
+
+      resp_json["name"].should == name
+      resp_json["description"].should == desc
+      
+      
+    end
+    it "saves in the db" do
+
+      count = Business.count
+      
+      post :create, :business=> business, :format => :json
+
+      Business.count.should > count
+      
     end
   end
 end
