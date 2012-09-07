27049308b439ee4636621bfb02b916e16b511ea1
Mick Thompson <dthompson@gmail.com>
2012-09-07 12:43:42 -0700

    add business classifications sic lookup

--- app/controllers/sic_controller.rb
+++ app/controllers/sic_controller.rb
@@ -3,10 +3,32 @@ require 'fuzzystringmatch'
 
 class SicController < ApplicationController
   def index
+    business_class = CsvMapper.import(Rails.root + 'app/assets/csv/business_class.csv') do
+      start_at_row 1
+      [bcc, classifcation, business_type, sic]
+    end
+
+    
     results = CsvMapper.import(Rails.root + 'app/assets/csv/zoning_and_sic_codes.csv') do
       start_at_row 1
-      [industry, subtype, code, industry_subtype, recheck, sic_name, sic_code, alternate_code, query_distance]
+      [industry, subtype, code, industry_subtype, recheck, sic_name, sic_code, alternate_code, query_distance, classifcation]
     end
+
+
+    results.each { |result|
+
+      if !result.alternate_code.nil?
+        result.sic_code = result.alternate_code
+      end
+      business_class.each { |bclass|
+        if !result.sic_code.nil? and bclass.sic.index(result.sic_code) == 0
+          result.classifcation = bclass.classifcation
+        end
+      }
+    }
+        
+      
+    
     query = params['q']
     query_distances = Array.new(results.length)
 
