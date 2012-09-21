c43a082abaf21ba4e63542fafd8a3580f99322a0
Serena Wales <serenawr@gmail.com>
2012-09-21 15:14:31 -0700

    Change phone number column type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120921215004) do
+ActiveRecord::Schema.define(:version => 20120921221142) do
 
   create_table "answers", :force => true do |t|
     t.text     "value"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(:version => 20120921215004) do
     t.string   "token"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "role"
   end
 
