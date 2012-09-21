4e9c3154bb6c2970db149aca0cc819d260f67ea5
Serena Wales <serenawr@gmail.com>
2012-09-21 16:07:50 -0700

    Make field_name accessible

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :value, :field_id, :business_id
+  attr_accessible :value, :field_name, :business_id
 
   belongs_to :field, :primary_key => :name, :foreign_key => :field_name
   belongs_to :business
