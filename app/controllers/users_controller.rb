cc20d9d3d51cef1811f6a34a3091decd53fedfcb
Serena Wales <serenawr@gmail.com>
2012-09-21 17:58:55 -0700

    Plurals are good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ActionController::Base
     @user = User.find_or_initialize_by_email(params[:user][:email])
     if @user.update_attributes(params[:user])
       session[:user_id] = @user.id
+      @current_user = @user
       @user.businesses << Business.create()
     end
     respond_with @user
