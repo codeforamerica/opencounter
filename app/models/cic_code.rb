ca08e6ae5ea66953bbe3fe670be922a795134823
Joel Mahoney <joelmahoney@gmail.com>
2012-09-26 14:14:08 -0700

    Add CicCode model

---
+++ app/models/cic_code.rb
@@ -0,0 +1,18 @@
+class CicCode < ActiveRecord::Base
+  has_many :cic_code_zoning_districts, :dependent => :destroy
+  has_many :zoning_districts, :through => :cic_code_zoning_districts, :dependent => :destroy
+  attr_accessible :code, :industry, :subindustry, :home_occ_prohibited, :keywords
+  after_create :create_zoning_district_connections
+  
+  def permission_name(zoning_district_id)
+    CicCodeZoningDistrict.cross_reference(self.id, zoning_district_id).try(:permission_name)    
+  end
+  
+  private
+  
+  def create_zoning_district_connections
+    ZoningDistrict.all.each do |zoning_district|
+      CicCodeZoningDistrict.find_or_create_by_cic_code_id_and_zoning_district_id(self.id, zoning_district.id)
+    end
+  end
+end
