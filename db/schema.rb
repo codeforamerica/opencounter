da9327ff7e4699ab144071a0b331d855cbec521e
Joel Mahoney <joelmahoney@gmail.com>
2012-09-26 14:16:36 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120921225738) do
+ActiveRecord::Schema.define(:version => 20120924221042) do
 
   create_table "answers", :force => true do |t|
     t.text     "value"
@@ -28,6 +28,32 @@ ActiveRecord::Schema.define(:version => 20120921225738) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "cic_code_zoning_districts", :force => true do |t|
+    t.integer  "cic_code_id"
+    t.integer  "zoning_district_id"
+    t.integer  "permission",          :default => 5
+    t.boolean  "home_occ_prohibited", :default => false
+    t.text     "notes"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "cic_code_zoning_districts", ["cic_code_id"], :name => "index_cic_code_zoning_districts_on_cic_code_id"
+  add_index "cic_code_zoning_districts", ["zoning_district_id"], :name => "index_cic_code_zoning_districts_on_zoning_district_id"
+
+  create_table "cic_codes", :force => true do |t|
+    t.string   "code"
+    t.string   "industry"
+    t.string   "subindustry"
+    t.text     "keywords"
+    t.boolean  "home_occ_prohibited", :default => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "cic_codes", ["code"], :name => "index_cic_codes_on_code"
+  add_index "cic_codes", ["keywords"], :name => "index_cic_codes_on_keywords"
 
   create_table "fields", :force => true do |t|
     t.string   "format"
@@ -60,5 +86,16 @@ ActiveRecord::Schema.define(:version => 20120921225738) do
     t.string   "phone"
     t.string   "role"
   end
+
+  create_table "zoning_districts", :force => true do |t|
+    t.string   "code"
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "home_occ_prohibited", :default => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "zoning_districts", ["code"], :name => "index_zoning_districts_on_code"
 
 end
