2ad700f9aa0389e0385e6b9a41dd6177dc17e32e
Joel Mahoney <joelmahoney@gmail.com>
2012-09-26 17:16:37 -0700

    Add inline forms for editing cic_code_zoning_district permissions

--- app/models/cic_code_zoning_district.rb
+++ app/models/cic_code_zoning_district.rb
@@ -1,7 +1,7 @@
 class CicCodeZoningDistrict < ActiveRecord::Base
   belongs_to :cic_code
   belongs_to :zoning_district
-  attr_accessible :cic_code_id, :home_occ_prohibited, :notes, :permitted, :permitted_aup, :permitted_sup, :prohibited, :unknown, :zoning_district_id
+  attr_accessible :cic_code_id, :home_occ_prohibited, :notes, :permission, :zoning_district_id
   
   def self.cross_reference(cic_code_id, zoning_district_id)
     self.where(cic_code_id: cic_code_id, zoning_district_id: zoning_district_id).try(:first)
