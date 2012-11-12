e137a3eccec048bd6263d8e6f71dde7d288b5a1e
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-11-12 23:26:39 +0000

    WIP admin/applications shows business name and applicant name

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -3,7 +3,15 @@ class Admin::ApplicationsController < ApplicationController
 
   def index
     # An application is defined as the collection of answers for a given user.
-    #@applications = User.all(&:answers)
+    # TODO: class? activerecord model?
+    @applications = User.all.map{ |u| [u,u.answers] }
+  end
+
+  def show
   end
 
 end
+
+
+# Lifted from Facets
+
