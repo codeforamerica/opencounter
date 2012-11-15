11427acb78ad40078f278d5803383a67a117cf61
Joel Mahoney <joelmahoney@gmail.com>
2012-11-15 13:01:51 -0800

    Add admin users to admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -7,4 +7,5 @@ class AdminUser < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
 end
