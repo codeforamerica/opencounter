983ede3062dc0d683fd06130f621c0aae6ca4ff7
Joel Mahoney <joelmahoney@gmail.com>
2012-11-16 09:50:26 -0800

    Send emails to Peter; add SendGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,17 @@ Opencounter::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # e-mail for help and form submission should go to this address
-  config.PLANNING_DEPARTMENT_EMAIL = 'planningcounter@cityofsantacruz.com'
+  config.PLANNING_DEPARTMENT_EMAIL = 'pkoht@cityofsantacruz.com'
+  
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
   
   # e-mail configuration for devise
   config.action_mailer.default_url_options = { :host => 'http://opencounter.cityofsantacruz.com/' }
