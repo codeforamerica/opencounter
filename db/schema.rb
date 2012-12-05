d6dae491c9f621c8d0813add4c1c4c3cb78da6aa
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-12-05 22:27:24 +0000

    fixed current_user helper method, added full dump of answers to admin/applications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120222310) do
+ActiveRecord::Schema.define(:version => 20121205214719) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -168,11 +168,14 @@ ActiveRecord::Schema.define(:version => 20121120222310) do
     t.string   "last_state"
     t.string   "email"
     t.string   "token"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "phone"
     t.string   "role"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
   create_table "zoning_districts", :force => true do |t|
     t.string   "code"
