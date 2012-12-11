0942f8b7c82d46709603b4b82ddde7fb0c842d82
Joel Mahoney <joelmahoney@gmail.com>
2012-12-10 22:41:04 -0800

    Add answers field_name value helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def answer_value(user, key_name)
+    if user.blank? || user.answers.blank?
+      return ''
+    else
+      user.answers.find_by_field_name(key_name).try(:value)
+    end
+  end
 end
