1cb5b9b8b1a8b65de4d006384ff9babb3e64832d
Joel Mahoney <joelmahoney@gmail.com>
2013-01-24 16:37:04 -0800

    Refactor application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def user_answer_value(user, key_name)
-    if user.try(:current_business).try(:answers).blank?
+    if user.try(:current_business).try(:answers).try(:find_by_field_name, key_name).try(:value).blank?
       return ''
     else
-      user.current_business.answers.find_by_field_name(key_name).try(:value)
+      user.current_business.answers.find_by_field_name(key_name).value
     end
   end
 end
